8ff7a7b164523e83a50f72a6718cbe34c3d41445
jayzz55 <jayzzwijono@yahoo.com>
2015-04-16 21:48:23 +1000

    ready to deploy to heroku

--- config.rb
+++ config.rb
@@ -2,3 +2,11 @@ ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :database => 'savvybudget'
 )
+
+local_db = {
+  :adapter => 'postgresql',
+  :database => 'savvybudget'
+}
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || local_db)
+ActiveRecord::Base.logger = Logger.new(STDERR) #show sql in the terminal
